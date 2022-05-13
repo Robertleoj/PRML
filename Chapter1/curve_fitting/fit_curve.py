3eeafc4a4184b930452c15f29038b8dd8f5094c5
Robertleoj <robert.leo.jonsson@gmail.com>
2022-05-13 01:02:46 +0000

    implement exact solution

--- Chapter1/curve_fitting/fit_curve.py
+++ Chapter1/curve_fitting/fit_curve.py
@@ -26,75 +26,107 @@ plt_y = data_func(plt_x)
 plt.plot(plt_x, plt_y)
 plt.show()
 
-
-# Cell
-M = 10
-
-def model(x: torch.Tensor, w:torch.Tensor):
+def get_stacked(x, M):
     x_stacked = torch.stack(
         tuple((x ** i).view(-1) for i in range(M + 1)),
         dim=1
     )
-    return x_stacked @ w
+    return x_stacked
 
 # Cell
-# first try fitting with gradient descent
-w = torch.randn((M + 1, 1))
-w.requires_grad = True
-
-plt.plot(x, model(x, w).detach())
+def model(x_stacked: torch.Tensor, w:torch.Tensor):
+    return x_stacked @ w
 
 # Cell
 def mean_squared_err(preds, targets):
     return ((preds - targets) ** 2).sum() / 2
 
 # Cell
-preds = model(x, w)
-mean_squared_err(preds, y)
+def solve_grad(x, y, M):
+    w = torch.randn((M + 1, 1))
+    w.requires_grad = True
+
+    n_epochs = 10000
+    lr = 0.01
+
+    x_stacked = get_stacked(x, M)
+
+    def update_weights(w, lr):
+        with torch.no_grad():
+            w -= lr * w.grad
+
+    def zero_grad(w):
+        if w.grad is not None:
+            w.grad.zero_()
+
+    for i in range(n_epochs):
+
+        zero_grad(w)
+
+        preds = model(x_stacked, w)
+
+        loss = mean_squared_err(preds, y)
+        loss.backward()
+        update_weights(w, lr)
+
+        if i % (n_epochs // 10) == 0:
+            print(f'Epoch {i}/{n_epochs - 1}')
+            print(f'\tLoss: {loss}')
+
+    return w
+
 # Cell
-n_epochs = 100000
-lr = 0.01
+w_grad = solve_grad(x, y, 10)
 
-def update_weights(w, lr):
-    with torch.no_grad():
-        w -= lr * w.grad
+# Cell
 
-def zero_grad(w):
-    if w.grad is not None:
-        w.grad.zero_()
+def closed_form(x: torch.Tensor, y:torch.Tensor, M:int):
+    # Make T
+    x_flat = x.reshape(-1)
+    x_stacked = torch.stack(
+        tuple(x_flat ** i for i in range(M + 1)),
+        dim = 0
+    )
+    T = x_stacked @ y.reshape(-1, 1)
 
-for i in range(n_epochs):
+    # Make A
+    A_exponents = torch.arange(0, M + 1, 1) + torch.arange(0, M + 1, 1).reshape(-1, 1)
+    A_exponents = A_exponents.reshape(M + 1, M + 1, 1)
 
-    zero_grad(w)
+    A_xs = torch.ones(M + 1, M + 1, x.size(1)) * x.reshape(-1)
+    A = (A_xs ** A_exponents).sum(2)
 
-    preds = model(x, w)
+    # return A, T
 
-    loss = mean_squared_err(preds, y)
-    loss.backward()
-    update_weights(w, lr)
+    w = torch.linalg.solve(A, T)
+    return w
 
-    if i % (n_epochs // 10) == 0:
-        print(f'Epoch {i}/{n_epochs - 1}')
-        print(f'\tLoss: {loss}')
-
-
-
-    
+# Cell
+w_exact = closed_form(x, y, 5)
+# A.shape, T.shape
 
 
 # Cell
 def plot_results(x: torch.Tensor, y: torch.Tensor, w:torch.Tensor):
+    M = w.size(0) - 1
+    # x_stacked = get_stacked(x, M)
+
     plt.scatter(x.numpy(), y.numpy())
     X_plt = torch.linspace(0, 1, 100)
+    X_plt_stacked = get_stacked(X_plt, M)
     with torch.no_grad():
-        model_out = model(X_plt, w)
-    plt.plot(X_plt, model_out, label="predicted")
+        model_out = model(X_plt_stacked, w)
+
+    plt.plot(X_plt, model_out.reshape(-1), label="predicted")
     plt.plot(X_plt, data_func(X_plt), label="data distribution")
     plt.legend()
 
     plt.show()
 
 # Cell
-plot_results(x, y, w)
+plot_results(x, y, w_grad)
+
+# Cell
+plot_results(x, y, w_exact)
 
 # Cell
